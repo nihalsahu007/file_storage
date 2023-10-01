f6feed53ff17ce8698cd44de5b73983903691ad7
nihal <49619696+nihalsahu007@users.noreply.github.com>
2023-10-01 17:58:28 +0530

    Update settings.py
    change password

--- file_storage/file_storage/settings.py
+++ file_storage/file_storage/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "nihalgopaani@gmail.com"
-EMAIL_HOST_PASSWORD = "pipbzwbfjvvowrfj"
+EMAIL_HOST_PASSWORD = "password"
